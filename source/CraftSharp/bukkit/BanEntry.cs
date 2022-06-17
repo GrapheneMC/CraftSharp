3481e42c7968add2d087393b26ad64703bd5a960
John <jwelshoffical78@gmail.com>
2022-06-16 19:24:51 -0500

    Implement 'BanEntry' and 'BanList'

---
+++ source/CraftSharp/bukkit/BanEntry.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftSharp.bukkit;
+
+/* TODO: long original comment up here I'll look into adding it later */
+public interface BanEntry
+{
+    /// <summary>
+    /// Gets the target involved. This may be in the form of an IP or a player name.
+    /// </summary>
+    /// <returns>the target name or IP address</returns>
+    public string GetTarget();
+
+    /// <summary>
+    /// Gets the date this ban entry was created.
+    /// </summary>
+    /// <returns>the creation date</returns>
+    public DateTime GetCreated();
+
+    /// <summary>
+    /// Sets the date this ban entry was created.
+    /// </summary>
+    /// <param name="created">the new created date, cannot be null</param>
+    /// <seealso cref="save"/>
+    public void SetCreated(DateTime created);
+
+    /// <summary>
+    /// Gets the source of this ban.
+    /// 
+    /// Note: A source is considered any String, although this is generally a  player name.
+    /// </summary>
+    /// <returns>the source of the ban</returns>
+    public string GetSource();
+
+    /// <summary>
+    /// Sets the source of this ban.
+    ///  
+    /// Note: A source is considered any String, although this is generally a player name.
+    /// </summary>
+    /// <param name="source">the new source where null values become empty strings</param>
+    /// <seealso cref="save"/>
+    public void SetSource(string source);
+
+    /// <summary>
+    ///  Gets the date this ban expires on, or null for no defined end date.
+    /// </summary>
+    /// <returns>the expiration date</returns>
+    public DateTime GetExpiration();
+
+    /// <summary>
+    /// Sets the date this ban expires on. Null values are considered "infinite" bans.
+    /// </summary>
+    /// <param name="expiration">expiration the new expiration date, or null to indicate an eternity</param>
+    /// <seealso cref="save"/>
+    public void SetExpiration(DateTime expiration);
+
+    /// <summary>
+    /// Gets the reason for this ban.
+    /// </summary>
+    /// <returns>the ban reason, or null if not set</returns>
+    public string GetReason();
+
+    /// <summary>
+    /// Sets the reason for this ban. Reasons must not be null.
+    /// </summary>
+    /// <param name="reason">reason the new reason, null values assume the implementation default</param>
+    /// <seealso cref="save"/>
+    public void SetReason(string reason);
+
+    /// <summary>
+    /// Saves the ban entry, overwriting any previous data in the ban list.
+    ///  
+    /// Saving the ban entry of an unbanned player will cause the player to be banned once again.
+    /// </summary>
+    public void save();
+}
---
+++ source/CraftSharp/bukkit/BanList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftSharp.bukkit;
+
+/// <summary>
+///  A ban list, containing bans of some
+///  <see cref="Type"/>
+/// </summary>
+public interface BanList
+{
+    /// <summary>
+    /// Represents a ban-type that a <see cref="BanList"/> may track.
+    /// </summary>
+    public enum Type
+    {
+        /// <summary>
+        /// Banned player names
+        /// </summary>
+        NAME,
+        /// <summary>
+        /// Banned player IP addresses
+        /// </summary>
+        IP,
+    }
+
+    /// <summary>
+    /// Gets a <see cref="BanEntry"/> by target.
+    /// </summary>
+    /// <param name="target">entry parameter to search for</param>
+    /// <returns> the corresponding entry, or null if none found</returns>
+    public BanEntry GetBanEntry(string target);
+
+    /// <summary>
+    /// Adds a ban to the this list. If a previous ban exists, this will update the previous entry.
+    /// </summary>
+    /// <param name="target">the target of the ban</param>
+    /// <param name="reason">reason for the ban, null indicates implementation default</param>
+    /// <param name="expires">date for the ban's expiration (unban), or null to imply forever</param>
+    /// <param name="source"> source of the ban, null indicates implementation default</param>
+    /// <returns>the entry for the newly created ban, or the entry for the (updated) previous ban</returns>
+    public BanEntry AddBan(string target, string reason, DateTime expires, string source);
+
+    /// <summary>
+    /// Gets a set containing every <see cref="BanEntry"/> in this list.
+    /// </summary>
+    /// <returns> an immutable set containing every entry tracked by this list</returns>
+    public HashSet<BanEntry> GetBanEntries();
+
+    /// <summary>
+    /// Gets if a <see cref="BanEntry"/> exists for the target, indicating an active ban status.
+    /// </summary>
+    /// <param name="target">the target to find</param>
+    /// <returns>true if a <see cref="BanEntry"/> exists for the name, indicating an active ban status, false otherwise</returns>
+    public bool IsBanned(string target);
+
+    /// <summary>
+    /// Removes the specified target from this list, therefore indicating a "not banned" status.
+    /// </summary>
+    /// <param name="target">the target to remove from this list</param>
+    public void Pardon(string target);
+}
